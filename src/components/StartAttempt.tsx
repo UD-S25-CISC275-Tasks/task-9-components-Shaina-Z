0442ba9d164a375030147306ba1226f7f1c36c72
Shaina <shainaz@udel.edu>
2025-03-04 22:00:39 -0500

    working on start attempt

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,10 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): React.JSX.Element {
+    const [value, setValue] = useState<number>(0);
+    const [inProgress, setProgress] = useState<boolean>(false);
+    <span>
+            <Button onClick={() => setValue(1 + value)}>Mulligan</Button>
+        </span>
     return <div>Start Attempt</div>;
 }
