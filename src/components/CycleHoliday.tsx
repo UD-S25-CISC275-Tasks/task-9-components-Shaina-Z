dc22229d27873f8854febd429a3642e3fdea6280
Shaina <shainaz@udel.edu>
2025-03-06 10:42:46 -0500

    added cycleAlphabetically

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,8 +2,44 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): React.JSX.Element {
-    const [holiday, setHoliday] = useState<string>("");
-    function cycleAlphabetically() {}
-    function cycleYear() {}
-    return <div>Holiday: {holiday}</div>;
+    const [holiday, setHoliday] = useState<string>("♥️");
+    function cycleAlphabetically() {
+        if (holiday === "🎄") {
+            setHoliday("🇺🇸");
+        } else if (holiday === "🇺🇸") {
+            setHoliday("🎃");
+        } else if (holiday === "🎃") {
+            setHoliday("🍀");
+        } else if (holiday === "🍀") {
+            setHoliday("♥️");
+        } else if (holiday === "♥️") {
+            setHoliday("🎄");
+        }
+    }
+    function cycleYear() {
+        if (holiday === "🎄") {
+            setHoliday("♥️");
+        } else if (holiday === "♥️") {
+            setHoliday("🍀");
+        } else if (holiday === "🍀") {
+            setHoliday("🇺🇸");
+        } else if (holiday === "🇺🇸") {
+            setHoliday("🎃");
+        } else if (holiday === "🎃") {
+            setHoliday("🎄");
+        }
+    }
+    return (
+        <div>
+            <span>
+                <Button onClick={cycleYear}>Advance By Year</Button>
+            </span>
+            <span>
+                <Button onClick={cycleAlphabetically}>
+                    Advance By Alphabet
+                </Button>
+            </span>
+            <span>Holiday: {holiday}</span>
+        </div>
+    );
 }
